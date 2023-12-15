88b128fc5f77b59013b8672c71e53891164812b3
Masonord <trotf233@gmail.com>
2023-12-15 22:27:11 +0300

    add functionality to respond to different screen sizes

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,12 +1,28 @@
 import React, {useState} from 'react';
-import styles from "../../styles/navbar.module.css"
+import styles from "../../styles/navbar.module.css";
+import { getImageUrl } from '../../utils/load.image';
+
+import menuItem from '../../../assets/nav/menuIcon.png';
+import closeItem from '../../../assets/nav/closeIcon.png';
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     return <nav className={styles.navBar}>
         <a className = {styles.title}
             href='/'>Portfolio
         </a>
         <div className = {styles.menu}>
+            <img
+                className = {styles.menuBtn}
+                // src = {getImageUrl('/nav/menuicon.png')}
+                src = {
+                    menuOpen
+                        ? closeItem
+                        : menuItem
+                }
+                alt = "menu-button"
+                onClick = {() => setMenuOpen(!menuOpen)}
+            />
             <ul className= {styles.menuItems}>
                 <li>
                     <a href='#about'>About</a>
