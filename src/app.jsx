de8211f4991037ea36cf5ef5a9119cbe1590dc4d
Masonord <trotf233@gmail.com>
2024-01-05 01:04:40 +0300

    add smooth scrolling to the page

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import styles from "./styles/app.module.css"
+import './styles/index.css'
 import { Navbar } from './components/navbar/navbar'
 import { Bio } from './components/hero/bio'
+import About from './components/about/about'
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar/>
       <Bio/>
+      <About/>
     </div>
   )
 }
